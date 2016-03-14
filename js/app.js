ee93ebf8a18df188da71c2ef01061702c583eda4
Dustin <leedu708@gmail.com>
2016-03-14 18:24:57 -0400

    add resume pdf

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var leedu = angular.module('leedu', ['ui.router', 'ngAnimate']);
+var leedu = angular.module('leedu', ['ui.router', 'ngAnimate', 'ngSanitize']);
 
 leedu.config( function($stateProvider, $urlRouterProvider) {
 
--- js/controllers/SkillsCtrl.js
+++ js/controllers/SkillsCtrl.js
@@ -3,5 +3,7 @@ leedu.controller('SkillsCtrl',
   function($scope, $rootScope) {
 
     $rootScope.title = 'Skills';
+    $scope.header = 'Skills';
+    $scope.resumeURL = "<a href='/files/resume.pdf'>Download as PDF</a>"
 
   }]);
