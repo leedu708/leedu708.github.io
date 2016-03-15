972b1934f50d0d630c7a995d52f5ec4c501950df
Dustin <leedu708@gmail.com>
2016-03-14 21:01:23 -0400

    resume added

--- js/controllers/SkillsCtrl.js
+++ js/controllers/SkillsCtrl.js
@@ -6,17 +6,17 @@ leedu.controller('SkillsCtrl',
     $scope.education = {
       title: 'Education',
       schools: [
-        'Cornell University, College of Agriculture and Life Sciences',
-        'Viking Code School'
-        ],
-      locations: [
-        'Ithaca, NY',
-        ''
-        ],
-      descriptions: [
-        'Bachelor of Science in Biological Engineering, January 2015',
-        'Major technologies taught include: Ruby, Ruby on Rails, AngularJS, SQL, Data Modeling, HTML5, CSS3, JavaScript, RSpec, and Git.  Completed December 2015'
-        ]
+        {
+          name: 'Cornell University, College of Agriculture and Life Sciences',
+          location: 'Ithaca, NY',
+          description: 'Bachelor of Science in Biological Engineering, January 2015'
+        },
+        {
+          name: 'Viking Code School',
+          location: '',
+          description: 'Major technologies taught include: Ruby, Ruby on Rails, AngularJS, SQL, Data Modeling, HTML5, CSS3, JavaScript, RSpec, and Git.  Completed December 2015'
+        }
+      ]
     };
 
     $scope.skills = {
@@ -57,7 +57,7 @@ leedu.controller('SkillsCtrl',
           location: 'Elmhurst, NY',
           time: '2013 - Present',
           title: 'Tech Support Administrator',
-          description: [
+          bullets: [
             'Prepare documentation of Electronic Medical Record (EMR) system for known errors',
             'Install and maintain local server for remote access to patient profiles'
             ]
@@ -67,8 +67,8 @@ leedu.controller('SkillsCtrl',
           location: 'Ithaca, NY',
           time: '1/2015 - 5/2015',
           title: 'Research Assistant, Cornell BEE Program',
-          description: [
-            'Organized and analyzed data from a simulated Mars alnding using Matlab',
+          bullets: [
+            'Organized and analyzed data from a simulated Mars landing using Matlab',
             'Authored documentation for the research program: HI-SEAS Documentation'
             ]
         },
@@ -77,7 +77,7 @@ leedu.controller('SkillsCtrl',
           location: 'New York, NY',
           time: '2008 - 2013',
           title: 'Lead Research Assistant, Asian Services Program',
-          description: [
+          bullets: [
             'Surveyed patients in English and Cantonese',
             'Analyze and organize data on patients using Excel forms'
             ]
@@ -86,8 +86,8 @@ leedu.controller('SkillsCtrl',
           workplace: 'Law Office of Zelda L. Lee, P.C.',
           location: 'Queens, NY',
           time: '2010 - 2015',
-          title: '',
-          description: [
+          title: 'Tech Support Administrator',
+          bullets: [
             'Installed VNC server and security system for remote viewing',
             'Installed centralized fax server'
             ]
