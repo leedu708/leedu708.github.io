dcc63b531c0ecd366d1eec396327a0ba69474bf0
leedu708 <leedu708@gmail.com>
2016-03-16 15:16:17 -0400

    adjusted about layout and added ipsum to controller

--- js/controllers/AboutCtrl.js
+++ js/controllers/AboutCtrl.js
@@ -4,7 +4,25 @@ leedu.controller('AboutCtrl',
 
     $rootScope.title = 'About';
 
-    $scope.profileImage = '/images/placeholder.png'
-    $scope.about = 'Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  Testing 1 2 3.  '
+    $scope.aboutContent = [
+      {
+        img: '/images/placeholder.png',
+        imgRight: false,
+        title: 'Title',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis nunc ultrices libero vehicula hendrerit id sed risus. Morbi sed tristique magna. Curabitur commodo ultricies nisi et lacinia. Nunc vel dapibus felis, quis faucibus dolor. Aliquam erat volutpat. Donec commodo metus a orci consectetur lobortis. Integer condimentum urna et ipsum rutrum, nec auctor nibh tincidunt. Nam id ipsum mi. In ullamcorper varius dui, id feugiat metus iaculis et. Integer tincidunt nulla commodo, molestie turpis a, sagittis urna.'
+      },
+      {
+        img: '/images/placeholder.png',
+        imgRight: true,
+        title: 'Title',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis nunc ultrices libero vehicula hendrerit id sed risus. Morbi sed tristique magna. Curabitur commodo ultricies nisi et lacinia. Nunc vel dapibus felis, quis faucibus dolor. Aliquam erat volutpat. Donec commodo metus a orci consectetur lobortis. Integer condimentum urna et ipsum rutrum, nec auctor nibh tincidunt. Nam id ipsum mi. In ullamcorper varius dui, id feugiat metus iaculis et. Integer tincidunt nulla commodo, molestie turpis a, sagittis urna.'
+      },
+      {
+        img: '/images/placeholder.png',
+        imgRight: false,
+        title: 'Title',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis nunc ultrices libero vehicula hendrerit id sed risus. Morbi sed tristique magna. Curabitur commodo ultricies nisi et lacinia. Nunc vel dapibus felis, quis faucibus dolor. Aliquam erat volutpat. Donec commodo metus a orci consectetur lobortis. Integer condimentum urna et ipsum rutrum, nec auctor nibh tincidunt. Nam id ipsum mi. In ullamcorper varius dui, id feugiat metus iaculis et. Integer tincidunt nulla commodo, molestie turpis a, sagittis urna.'
+      }
+    ];
 
   }]);
