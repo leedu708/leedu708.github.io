60d60797ddc193ed4caefaf34709dcaa6c06274f
leedu708 <leedu708@gmail.com>
2016-03-15 21:53:29 -0400

    add photos to portfolio

--- js/controllers/PortfolioCtrl.js
+++ js/controllers/PortfolioCtrl.js
@@ -7,37 +7,37 @@ leedu.controller('PortfolioCtrl',
     $scope.projects = [
       {
         title: 'Evergreen',
-        img: '/images/placeholder.png',
+        img: '/images/evergreen.jpg',
         github: 'https://github.com/leedu708/evergreen',
         preview: 'http://fast-shore-10637.herokuapp.com/'
       },
       {
         title: 'Danebook',
-        img: '/images/placeholder.png',
+        img: '/images/danebook.png',
         github: 'https://github.com/leedu708/danebook',
         preview: 'http://blooming-tundra-4486.herokuapp.com/'
       },
       {
         title: 'Djello',
-        img: '/images/placeholder.png',
+        img: '/images/djello.png',
         github: 'https://github.com/leedu708/project_djello',
         preview: 'http://djl-djello.heroku.com/'
       },
       {
         title: 'Tetris',
-        img: '/images/placeholder.png',
+        img: '/images/tetris.png',
         github: 'https://github.com/leedu708/project_tetris_js',
         preview: 'https://htmlpreview.github.io/?https://github.com/leedu708/project_tetris_js/blob/master/tetris.html'
       },
       {
-        title: 'Fideligard',
-        img: '/images/placeholder.png',
-        github: 'https://github.com/leedu708/assignment_fideligard_spa',
-        preview: ''
+        title: 'Uber Challenge',
+        img: '/images/uber.png',
+        github: 'https://github.com/leedu708/uber_food_truck_challenge',
+        preview: 'https://pure-mountain-6560.herokuapp.com/'
       },
       {
         title: "Asteroids",
-        img: '/images/placeholder.png',
+        img: '/images/asteroids.png',
         github: 'https://github.com/leedu708/assignment_js_oop',
         preview: 'https://htmlpreview.github.io/?https://github.com/leedu708/assignment_js_oop/blob/master/index.html'
       }
